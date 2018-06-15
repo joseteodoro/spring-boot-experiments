7c676be9ed0e003fe5e6541bd56422f2dbcd27ab
joseteodoro <jteodoro.dev@gmail.com>
2018-06-14 21:59:20 -0300

    filling test with spring-boot-webflux

--- rest-service/src/test/java/br/josets/jdk10/restservice/RestServiceApplicationTests.java
+++ rest-service/src/test/java/br/josets/jdk10/restservice/RestServiceApplicationTests.java
@@ -2,15 +2,30 @@ package br.josets.jdk10.restservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.josets.jdk10.restservice.hello.Greeting;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RestServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webClient;
+	
 	@Test
-	public void contextLoads() {
+	public void exampleTest() {
+		this.webClient.get()
+			.uri("/greeting")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody(Greeting.class).isEqualTo(new Greeting(1, "Hello, World!"));
 	}
 
+	
 }
