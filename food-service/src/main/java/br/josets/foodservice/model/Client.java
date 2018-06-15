db745b6610251d5bd127d79a3246a30b32783ef1
joseteodoro <jteodoro.dev@gmail.com>
2018-06-15 16:36:08 -0300

    adding initial version of food-service

---
+++ food-service/src/main/java/br/josets/foodservice/model/Client.java
@@ -0,0 +1,9 @@
+package br.josets.foodservice.model;
+
+public class Client {
+	
+	private long id;
+	
+	private String login;
+
+}
---
+++ food-service/src/main/java/br/josets/foodservice/model/Meal.java
@@ -0,0 +1,5 @@
+package br.josets.foodservice.model;
+
+public class Meal {
+
+}
